40c0bfcc507cd0fb43c6a73269e546f952abc91f
Kenneth S <kksiu@wisc.edu>
2014-10-11 20:19:47 -0500

    Initial commit

---
+++ SimpleWhiteboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleWhiteboard
+//
+//  Created by Kenneth Siu on 10/11/14.
+//  Copyright (c) 2014 Kenneth Siu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
